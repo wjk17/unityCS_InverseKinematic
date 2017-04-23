595197a481d494596d27954f477bb1b86f3d8f61
wjk <478286460@qq.com>
2017-04-23 11:05:31 +0800

    init

---
+++ Assets/IKTool.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class IKBoneChain // IK骨骼链
+{
+    public Joint[] joints;
+    public Transform end;
+    public Transform target;
+    public int iteration; // 计算的迭代次数
+    public float maxDeltaAngle;
+    public void Solve()
+    {
+        int i;
+        for (i = 0; i < iteration; i++)
+        {
+            foreach (var joint in joints) IKTool.IKSolve(joint, target, end, maxDeltaAngle);
+            //if (IKTool.Close(target.position, end.position)) { Debug.Log(i.ToString() + " 次迭代   完成"); return; }
+            if (IKTool.Close(target.position, end.position)) return;
+        }
+        //Debug.Log(i.ToString() + " 次迭代  未完成");
+    }
+    public WRay Solve(int i)
+    {
+        var result = IKTool.IKSolve(joints[i], target, end, maxDeltaAngle);
+        if (IKTool.Close(target.position, end.position)) Debug.Log("完成");
+        return result;
+    }
+}
+public class WRay
+{
+    public Vector3 pos;
+    public Vector3 dir;
+    public Color color;
+    public void Draw()
+    {
+        Debug.DrawRay(pos, dir, color);
+    }
+    public WRay(Vector3 pos, Vector3 dir, Color color)
+    {
+        this.pos = pos;
+        this.dir = dir * 3f;
+        this.color = color;
+    }
+}
+public static class IKTool
+{
+    public static float closest = 0.0001f;
+    public static bool Close(Vector3 a, Vector3 b)
+    {
+        return CloseZero((a - b).sqrMagnitude);
+    }
+    public static bool Close(float a, float b)
+    {
+        return Mathf.Approximately(a, b) || Mathf.Abs(a - b) <= closest;
+    }
+    public static bool CloseZero(float f)
+    {
+        return Mathf.Approximately(f, 0f) || float.IsNaN(f) || Mathf.Abs(f) <= closest;
+    }
+    /// <summary>
+    /// 计算骨骼链
+    /// </summary>
+    /// <param name="关节"></param>
+    /// <param name="目标位置"></param>
+    /// <param name="末端效应器"></param>
+    /// <param name="每次迭代最大旋转角度"></param>
+    /// <param name="迭代次数"></param>
+    public static WRay IKSolve(Joint joint, Transform target, Transform end, float limitAngle)
+    {
+        // 关节相对于目标位置和末端效应器的位置
+        Vector3 absJoint2End = end.position - joint.transform.position;
+        Vector3 absJoint2Target = target.position - joint.transform.position;
+
+        // 转为本地坐标系
+        Quaternion invRotation = joint.transform.rotation.Conjugate();
+
+        Vector3 localJoint2End = invRotation * absJoint2End;
+        Vector3 localJoint2Target = invRotation * absJoint2Target;
+        float deltaAngle = Mathf.Rad2Deg * Mathf.Acos(Vector3.Dot(localJoint2End.normalized, localJoint2Target.normalized));
+        if (CloseZero(deltaAngle)) return null;
+
+        Vector3 rotateAxis = Vector3.Cross(localJoint2Target, localJoint2End).normalized;
+        Quaternion deltaRotation = Quaternion.AngleAxis(deltaAngle, rotateAxis);
+        deltaRotation = deltaRotation.Conjugate();
+
+        var ray = new WRay(joint.transform.position, rotateAxis, Color.green);
+
+        var delta = deltaRotation.eulerAngles;
+        var cur = joint.transform.localRotation.eulerAngles;
+        var deltaV = Vector3.zero;
+        if ((joint.constraints & RigidbodyConstraints.FreezeRotationX) == 0)
+        {
+            deltaV += new Vector3(limit(delta.x, limitAngle), 0f, 0f);
+        }
+        if ((joint.constraints & RigidbodyConstraints.FreezeRotationY) == 0)
+        {
+            deltaV += new Vector3(0f, limit(delta.y, limitAngle), 0f);
+        }
+        if ((joint.constraints & RigidbodyConstraints.FreezeRotationZ) == 0)
+        {
+            deltaV += new Vector3(0f, 0f, limit(delta.z, limitAngle));
+        }
+        deltaRotation.eulerAngles = deltaV;
+        joint.transform.localRotation *= deltaRotation;
+        var euler = joint.transform.localRotation.eulerAngles;
+        var quat = new Quaternion();
+        quat.eulerAngles = Vector3.zero;
+
+        if ((joint.constraints & RigidbodyConstraints.FreezeRotationX) == 0)
+        {
+            quat.eulerAngles += new Vector3(Mathf.Clamp(euler.x, joint.minAngle, joint.maxAngle), 0f, 0f);
+        }
+        if ((joint.constraints & RigidbodyConstraints.FreezeRotationY) == 0)
+        {
+            quat.eulerAngles += new Vector3(0f, Mathf.Clamp(euler.y, joint.minAngle, joint.maxAngle), 0f);
+        }
+        if ((joint.constraints & RigidbodyConstraints.FreezeRotationZ) == 0)
+        {
+            quat.eulerAngles += new Vector3(0f, 0f, Mathf.Clamp(euler.z, joint.minAngle, joint.maxAngle));
+        }
+
+        joint.transform.localRotation = quat;
+        return ray;
+    }
+    public static float limit(float angle, float max)
+    {
+        bool inverse = false;
+        if (angle > 180)
+        {
+            angle = 360 - angle;
+            inverse = true;
+        }
+        angle = Mathf.Clamp(angle, -max, max);
+        return inverse ? -angle : angle;
+    }
+}
+
+[System.Serializable]
+public class Joint
+{
+    public RigidbodyConstraints constraints;
+    public float maxAngle;
+    public float minAngle;
+    public Transform transform;
+    public Joint(Transform transform)
+    {
+        this.transform = transform;
+        constraints = RigidbodyConstraints.None;
+    }
+    public Joint(Transform transform, RigidbodyConstraints constraints)
+    {
+        this.transform = transform;
+        this.constraints = constraints;
+    }
+}
+
+public static class QuaternionTool
+{
+    public static Quaternion Conjugate(this Quaternion q)
+    {
+        return new Quaternion(-q.x, -q.y, -q.z, q.w);
+    }
+}
---
+++ Assets/WodeIK.cs
@@ -0,0 +1,278 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WodeIK), true)]
+public class WodeIKEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        var ik = (WodeIK)target;
+        if (GUILayout.Button("初始化"))
+        {
+            ik.OnEnable();
+        }
+        if (GUILayout.Button("跟随"))
+        {
+            ik.Follow();
+        }
+        if (GUILayout.Button("缓慢跟随"))
+        {
+            ik.Lerp();
+        }
+        if (GUILayout.Button("计算关节链（*迭代次数）"))
+        {
+            ik.SolveAll();
+        }
+        if (GUILayout.Button("计算下一个关节"))
+        {
+            ik.SolveStep();
+        }
+    }
+}
+[ExecuteInEditMode]
+public class WodeIK : MonoBehaviour
+{
+    IKBoneChain chain;
+    public List<Transform> joints;
+    public Transform end;
+    public Transform target;
+    private Transform _n;
+    private Transform n
+    {
+        get
+        {
+            if (_n == null)
+            {
+
+                _n = (new GameObject("Lerp")).transform;
+                _n.position = target.position;
+                _n.rotation = target.rotation;
+            }
+            return _n;
+        }
+    }
+    private int index;
+    private WRay ray = null;
+    private bool follow;
+    private bool lerp;
+
+    [Header("参数预设")]
+    public SettingsPrefabType settingPrefabType = SettingsPrefabType.Hair;
+    private SettingsPrefabType _settingPrefabType = SettingsPrefabType.Hair;
+    private Setting[] settingPrefab;
+    public enum SettingsPrefabType
+    {
+        Hair,
+        CurlyHair,
+        Hand,
+        Tentacle,
+        Custom
+    }
+    public Setting setting;
+    private float _boneLength;
+    private int _jointCount;
+    [System.Serializable]
+    public struct Setting
+    {
+        [Header("骨骼长度")]
+        [Range(0, 50)]
+        public float boneLength;
+        [Header("关节数")]
+        [Range(1, 100)]
+        public int jointCount;
+        [Header("迭代次数")]
+        public int iteration;
+        [Header("每次迭代的最大旋转角度（相对值）")]
+        public float maxDeltaAngle;
+        [Header("关节最大角度（绝对值）")]
+        public float maxAngle;
+        [Header("关节最小角度（绝对值）")]
+        public float minAngle;
+        [Header("除了第一个关节以外只旋转X轴")]
+        public bool xOnly;
+    }
+    public void Lerp()
+    {
+        ray = null;
+        if (!lerp)
+        {
+            n.position = end.position;
+            chain.target = n;
+            follow = true;
+            lerp = true;
+        }
+        else
+        {
+            var timeMultiplier = 3.6f;
+            var minSpeed = 0.12f;
+            if ((target.position - n.position).sqrMagnitude < Mathf.Pow(minSpeed, 2))
+            {
+                n.position = target.position;
+                return;
+            }
+            else
+            {
+                var lerp = Vector3.Lerp(n.position, target.position, Time.deltaTime * timeMultiplier);
+                var x = lerp - n.position;
+                if (x.sqrMagnitude < Mathf.Pow(minSpeed, 2))
+                {
+                    n.position += x.normalized * minSpeed;
+                }
+                else n.position = lerp;
+            }
+
+        }
+    }
+    public void Follow()
+    {
+        chain.target = target;
+        follow = true;
+    }
+    public void UpdateValues()
+    {
+        UpdateJoints();
+        if (_boneLength != setting.boneLength)
+        {
+            _boneLength = setting.boneLength;
+            ScaleChain();
+        }
+        int i = 0;
+        foreach (var joint in chain.joints)
+        {
+            joint.maxAngle = setting.maxAngle;
+            joint.minAngle = setting.minAngle;
+            if (setting.xOnly)
+            {
+                joint.constraints = (i == chain.joints.Length - 1 ? RigidbodyConstraints.None :
+                    RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ);
+            }
+            else { joint.constraints = RigidbodyConstraints.None; }
+            i++;
+        }
+        chain.maxDeltaAngle = setting.maxDeltaAngle;
+        chain.iteration = setting.iteration;
+    }
+    GameObject origin { get { if (_origin == null) _origin = GameObject.Find("Joint"); return _origin; } }
+    GameObject _origin;
+    void ChangeChain()
+    {
+        if (origin == null) return;
+        if (_jointCount == setting.jointCount) return;
+        _jointCount = setting.jointCount;
+        UpdateChain();
+        if (joints.Count < _jointCount)
+        {
+            for (int i = joints.Count; i < _jointCount; i++)
+            {
+                var n = Instantiate(origin, joints[i - 1]).transform;
+                n.name = "Joint " + (i + 1).ToString();
+                joints.Add(n);
+            }
+            end.SetParent(joints[joints.Count - 1]);
+        }
+        else if (joints.Count > _jointCount)
+        {
+            end.SetParent(joints[_jointCount - 1]);
+            DestroyImmediate(joints[_jointCount].gameObject);
+
+        }
+        else { return; }
+        UpdateChain();
+        ScaleChain();
+    }
+    void ScaleChain()
+    {
+        for (int i = 0; i < joints.Count; i++)
+        {
+            var bone = joints[i].FindChild("Bone");
+            bone.localPosition = new Vector3(0, 0, setting.boneLength * 0.5f + 0.5f);
+            bone.localScale = new Vector3(1, 1, setting.boneLength);
+            bone.GetComponent<MeshRenderer>().enabled = !IKTool.CloseZero(bone.localScale.z);
+            joints[i].localPosition = new Vector3(0, 0, i == 0 ? 0 : 1 + setting.boneLength);
+            joints[i].localRotation = Quaternion.identity;
+        }
+        end.localPosition = new Vector3(0, 0, setting.boneLength + 0.5f + end.localScale.z * 0.5f);
+        end.SetParent(joints[joints.Count - 1]);
+    }
+    void UpdateChain()
+    {
+        var first = joints[0];
+        joints.Clear();
+        foreach (var joint in first.GetComponentsInChildren<SphereCollider>()) if (joint.transform != end) joints.Add(joint.transform);
+    }
+    public void Update()
+    {
+        if (_settingPrefabType != settingPrefabType)
+        {
+            _settingPrefabType = settingPrefabType;
+            if (settingPrefabType != SettingsPrefabType.Custom)
+            {
+                setting = settingPrefab[(int)settingPrefabType];
+            }
+        }
+        else if (settingPrefabType != SettingsPrefabType.Custom && !setting.Equals(settingPrefab[(int)settingPrefabType]))
+        {
+            settingPrefabType = SettingsPrefabType.Custom;
+            _settingPrefabType = settingPrefabType;
+        }
+        ChangeChain();
+        UpdateValues();
+        if (lerp) Lerp();
+        if (follow) SolveAll();
+        if (ray != null) ray.Draw();
+    }
+    void UpdateJoints()
+    {
+        var l = new List<Joint>();
+        for (int i = joints.Count - 1; i >= 0; i--)
+        {
+            l.Add(new Joint(joints[i]));
+        }
+        chain.joints = l.ToArray();
+    }
+    public void DoReset()
+    {
+        chain = new IKBoneChain();
+        if (joints.Count == 0) return;
+        ChangeChain();
+        joints[0].transform.localPosition = Vector3.zero;
+        joints[0].parent.transform.localPosition = Vector3.zero;
+
+        chain.end = end;
+        chain.target = target;
+        ScaleChain();
+        UpdateValues();
+        lerp = false;
+        follow = false;
+        index = 0;
+        ray = null;
+    }
+    public void OnEnable()
+    {
+        settingPrefab = new Setting[] {
+        new Setting() { boneLength = 5,jointCount =50, iteration = 100, maxDeltaAngle = 0.1f,maxAngle =360 ,minAngle =-360, xOnly = true },
+        new Setting() { boneLength = 5,jointCount =50,iteration = 100, maxDeltaAngle = 360,maxAngle =30 ,minAngle =-30, xOnly = false },
+        new Setting() { boneLength = 50,jointCount =2,iteration = 20, maxDeltaAngle = 360,maxAngle =360 ,minAngle =-360, xOnly = false },
+        new Setting() { boneLength = 1.2f,jointCount =40,iteration = 20, maxDeltaAngle = 25,maxAngle =360 ,minAngle =-360, xOnly = false }};
+        if (target == null) return;
+        DoReset();
+        foreach (var t in FindObjectsOfType<Transform>()) if (t.name == "Lerp") DestroyImmediate(t.gameObject);
+    }
+
+    public void SolveStep()
+    {
+        UpdateValues();
+        ray = chain.Solve(index % chain.joints.Length);
+        index++;
+    }
+    public void SolveAll()
+    {
+        ray = null;
+        UpdateValues();
+        chain.Solve();
+    }
+}
+
